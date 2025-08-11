c37a0eca88fb3038d67a1f793b14d845b935e081
Prince527 <Prince527gaming@gmail.com>
2025-08-11 20:27:09 -0300

    Peazip: Add Intel

--- Casks/peazip.rb
+++ Casks/peazip.rb
@@ -1,8 +1,11 @@
 cask "peazip" do
-  version "10.6.0"
-  sha256 "ed96ba770465d2c6718f7396803b22aa472c17c6346a1a9eb358edcf90239547"
+  arch arm: "aarch64", intel: "x86_64"
 
-  url "https://github.com/peazip/PeaZip/releases/download/#{version}/peazip-#{version}.DARWIN.aarch64.dmg",
+  version "10.6.0"
+  sha256 arm:   "ed96ba770465d2c6718f7396803b22aa472c17c6346a1a9eb358edcf90239547",
+         intel: "7ab22c71909e223b18d740d6882a46478dad9aaa5269e744283df7911ba7678a"
+
+  url "https://github.com/peazip/PeaZip/releases/download/#{version}/peazip-#{version}.DARWIN.#{arch}.dmg",
       verified: "github.com/peazip/PeaZip/"
   name "PeaZip"
   desc "Free Zip / Unzip software and Rar file extractor"
