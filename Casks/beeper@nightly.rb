332af24f5e839451ffb33a247b4656ebf368ceba
Omar <serversmpsmtp@gmail.com>
2025-09-15 00:38:53 -0300

    beeper@nightly 4.1.199 (#68)
    Co-authored-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/beeper@nightly.rb
+++ Casks/beeper@nightly.rb
@@ -2,9 +2,9 @@ cask "beeper@nightly" do
   arch arm: "-arm64"
   livecheck_arch = on_arch_conditional arm: "arm64", intel: "x64"
 
-  version "4.1.198"
-  sha256 arm:   "ade689c2ae43937357422a5875adb4f14ae48408125588bb41b6110db2a6806d",
-         intel: "289c816240276a5f7c7e6f3ead4983a3cf07959a786d1b9cc92b09f4e6a14808"
+  version "4.1.199"
+  sha256 arm:   "ab2a8db7625a3b4259405f4e0e21f9ea2cab3923429d2217ef7c90ac1372e167",
+         intel: "772f3be4cd6c9b006b5311a0a912acc0243a8cb6dbfeed6c8d3df8ae8d4b6fb1"
 
   url "https://beeper-desktop.download.beeper.com/builds/Beeper%20Nightly-#{version}#{arch}-mac.zip"
   name "Beeper"
