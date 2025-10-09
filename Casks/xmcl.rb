f6b69a14a6b1040642b904e4a6f8c65258e47888
Twint Actions <serversmpsmtp@gmail.com>
2025-10-09 00:32:10 -0300

    xmcl 0.52.6 (#94)
    Co-authored-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/xmcl.rb
+++ Casks/xmcl.rb
@@ -1,9 +1,9 @@
 cask "xmcl" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.52.5"
-  sha256 arm:   "54031a96a40e5a4b0faeb56cf92470ef4dedb39960f1d322c24df2a101576a59",
-         intel: "21a3517c763d4c93e4db9453239ddc9a74e4b07641a2b1eba2cab4efe28eb2a4"
+  version "0.52.6"
+  sha256 arm:   "fa3cf20a104b54ee53fbde5c2c0b602b0c148583d004e1371b6bc3385f7e35cb",
+         intel: "634346c08cf405aaba8dfa42b404a4011a561952b344a9611ece3ee6de77cb1b"
 
   url "https://github.com/Voxelum/x-minecraft-launcher/releases/download/v#{version}/xmcl-#{version}-#{arch}.dmg",
       verified: "github.com/Voxelum/x-minecraft-launcher/"
