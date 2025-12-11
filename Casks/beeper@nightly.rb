eea5582ec950386cbeb781284d99abca9923a8c4
Twint Actions <serversmpsmtp@gmail.com>
2025-12-10 23:57:14 -0400

    beeper@nightly 4.2.336 (#157)
    Co-authored-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/beeper@nightly.rb
+++ Casks/beeper@nightly.rb
@@ -2,9 +2,9 @@ cask "beeper@nightly" do
   arch arm: "-arm64"
   livecheck_arch = on_arch_conditional arm: "arm64", intel: "x64"
 
-  version "4.2.332"
-  sha256 arm:   "884268af6bfc26dfb6705a76512fddca4216b316cf636062a97433ea41d891b0",
-         intel: "44d2acbab2725b79236682fa56fcecfd2e5b5911e8bc8179591a5d588dcece1f"
+  version "4.2.336"
+  sha256 arm:   "dcd1493f1acb5d5c04946f5fadce32e34d6dd44ed6de54116de2a56e0f43d4a5",
+         intel: "5b5b2153b3e1084e5ed4354e2a64cea90f8a9fb031f4ea9617f84224ccfa83fd"
 
   url "https://beeper-desktop.download.beeper.com/builds/Beeper%20Nightly-#{version}#{arch}-mac.zip"
   name "Beeper"
