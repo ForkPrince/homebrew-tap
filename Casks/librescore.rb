8acdc511eb6c61f7996e613c3edd0b9082a2d963
Twint Actions <serversmpsmtp@gmail.com>
2026-01-18 00:05:40 -0400

    librescore 6.0.21 (#199)
    Co-authored-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/librescore.rb
+++ Casks/librescore.rb
@@ -1,6 +1,6 @@
 cask "librescore" do
-  version "6.0.20"
-  sha256 "26de0686e02158942b41b3041b119e1fc02120113a25f7637e110a4d39450e8f"
+  version "6.0.21"
+  sha256 "8e6bc6234ff10fcc0d711970122e00b5bed369e279c6adcc0312c80d69939658"
 
   url "https://github.com/LibreScore/app-librescore/releases/download/v#{version}/LibreScore.dmg"
   name "LibreScore"
