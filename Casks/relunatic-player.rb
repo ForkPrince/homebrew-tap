a12a5d7d17a01c9e8c3ace7d4235425b2d9ecad6
Prince527 <prince527@duck.com>
2026-03-17 09:15:08 -0300

    Patch

--- Casks/relunatic-player.rb
+++ Casks/relunatic-player.rb
@@ -1,11 +1,13 @@
 cask "relunatic-player" do
-  version "1.1.0"
+  version "1.2.0"
   sha256 "e8e2db3578952ec8293ab0af319bccf7a5afd7796a98d66adb521cc862fd80d7"
 
   url "https://github.com/Prince527GitHub/Re-Lunatic-Player/releases/download/v#{version}/Re.Lunatic.Player.dmg"
   name "Re:Lunatic Player"
   desc "Gensokyo Radio app continuation of Lunatic Player"
   homepage "https://github.com/Prince527GitHub/Re-Lunatic-Player"
+
+  macos Monterey
 
   app "Re-Lunatic Player.app"
 end
