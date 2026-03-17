259d255332bae46999a99ec9fdd32b32647af584
Prince527 <prince527@duck.com>
2026-03-17 09:38:39 -0300

    Patch

--- Casks/relunatic-player.rb
+++ Casks/relunatic-player.rb
@@ -1,5 +1,5 @@
 cask "relunatic-player" do
-  version "1.2.0"
+  version "1.1.0"
   sha256 "e8e2db3578952ec8293ab0af319bccf7a5afd7796a98d66adb521cc862fd80d7"
 
   url "https://github.com/Prince527GitHub/Re-Lunatic-Player/releases/download/v#{version}/Re.Lunatic.Player.dmg"
