f6be30590de78136534fb32dc362601174e83bec
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-24 05:33:13 +0000

    tinyrdm 1.2.7

--- Casks/tinyrdm.rb
+++ Casks/tinyrdm.rb
@@ -1,9 +1,9 @@
 cask "tinyrdm" do
   arch arm: "arm64", intel: "intel"
 
-  version "1.2.6"
-  sha256 arm:   "6a9de72ebb6be5b4fce1ebec3186edf8bd1ed5e3221bede082479c6ffe03a61b",
-         intel: "ac69281ec7d943d9e315a87c152a02ccca3da4cbf030c55db53f2504aa3f7898"
+  version "1.2.7"
+  sha256 arm:   "aaebc58a1f97505743bf05f2ab1cfc5e7c3e5841d90266ad836eaab74435f1a3",
+         intel: "3d8e61fa474ae50b61e41a623841fcdd7629615fa105758f2840fff75fe857ad"
 
   url "https://github.com/tiny-craft/tiny-rdm/releases/download/v#{version}/TinyRDM_#{version}_mac_#{arch}.dmg",
       verified: "github.com/tiny-craft/tiny-rdm/"
@@ -11,13 +11,13 @@ cask "tinyrdm" do
   desc "Modern and colorful Redis GUI client"
   homepage "https://redis.tinycraft.cc/"
 
-  depends_on macos: ">= :big_sur"
-
-  app "Tiny RDM.app"
-
   livecheck do
     url :url
     strategy :github_latest
     regex(/v?(\d+(?:\.\d+)+)/)
   end
+
+  depends_on macos: ">= :big_sur"
+
+  app "Tiny RDM.app"
 end
