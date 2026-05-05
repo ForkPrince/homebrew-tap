90ab66045528a17fd5cb65bb09622d4fe6414aca
Twint Actions <serversmpsmtp@gmail.com>
2026-05-05 02:41:18 -0300

    equicord-installer 2.1.9-pre3 (#309)
    Co-authored-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/equicord-installer.rb
+++ Formula/equicord-installer.rb
@@ -1,8 +1,8 @@
 class EquicordInstaller < Formula
   desc "Installer for Equicord"
   homepage "https://github.com/Equicord/Equicord"
-  url "https://github.com/Equicord/Equilotl/archive/refs/tags/v2.1.9-pre2.tar.gz"
-  sha256 "ef67c130b0e9cfe15f5e897b3771ecc15454bc009822acd3168a309e63c42666"
+  url "https://github.com/Equicord/Equilotl/archive/refs/tags/v2.1.9-pre3.tar.gz"
+  sha256 "e55c0d779c1ba0a6aecf5b5cf28d62bfcd3c459cf8c260829ceb6b2df1d8210a"
   license "GPL-3.0-or-later"
 
   depends_on "go" => :build
