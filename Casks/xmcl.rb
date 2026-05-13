171fc3d015176bb184662cac34d810e85b66c2f9
Twint Actions <serversmpsmtp@gmail.com>
2026-05-13 03:27:16 -0300

    xmcl 0.55.0 (#321)
    Co-authored-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/xmcl.rb
+++ Casks/xmcl.rb
@@ -1,15 +1,17 @@
 cask "xmcl" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.54.5"
-  sha256 arm:   "77aabddc532340e910dcc94b28fcf4f46deed0096353425466b282da6b316df1",
-         intel: "15aec79daa2d9f119e38dfac863881f7b7c738d3cca57b176f6dbfbebd63b22e"
+  version "0.55.0"
+  sha256 arm:   "4e94102fdad552e62c653dec3c238370f7ac5735b73ec6e6df9bcba8e443d03a",
+         intel: "7ad8b50adaccc9de92fd7b8dd1eb5e442d2cce1eea0508d8e65cd3b47a1b9606"
 
   url "https://github.com/Voxelum/x-minecraft-launcher/releases/download/v#{version}/xmcl-#{version}-#{arch}.dmg",
       verified: "github.com/Voxelum/x-minecraft-launcher/"
   name "X Minecraft Launcher"
   desc "Open Source Minecraft Launcher with Modern UX"
   homepage "https://xmcl.app/"
+
+  depends_on :macos
 
   app "X Minecraft Launcher.app"
 
