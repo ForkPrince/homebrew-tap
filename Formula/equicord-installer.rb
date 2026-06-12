c2a1042e78b8f46b972ef0ff6d77478b5c7c3562
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-12 07:20:58 +0000

    equicord-installer 2.2.6

--- Formula/equicord-installer.rb
+++ Formula/equicord-installer.rb
@@ -1,8 +1,8 @@
 class EquicordInstaller < Formula
   desc "Installer for Equicord"
   homepage "https://github.com/Equicord/Equicord"
-  url "https://github.com/Equicord/Equilotl/archive/refs/tags/v2.2.5.tar.gz"
-  sha256 "c2491129ee764084571bfac3b2d989f9ca39764824ae75895b4c15454c39271a"
+  url "https://github.com/Equicord/Equilotl/archive/refs/tags/v2.2.6.tar.gz"
+  sha256 "d7a43f9162e8f4a6dac0e6177925f2572faa316304942d6212abd314831ef131"
   license "GPL-3.0-or-later"
 
   depends_on "go" => :build
