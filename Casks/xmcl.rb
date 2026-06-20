64845f4135b1b11465951fbdcc83ae3c07d518e9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-20 07:00:23 +0000

    xmcl 0.57.0

--- Casks/xmcl.rb
+++ Casks/xmcl.rb
@@ -1,9 +1,9 @@
 cask "xmcl" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.56.8"
-  sha256 arm:   "7a37777c70394da526ac77e4848df89d5ef94e50d4e7545627c5b283671230ff",
-         intel: "caf42391e3bee19fa512eda4fad36459de98a00028f880c2319adbbff3260f2a"
+  version "0.57.0"
+  sha256 arm:   "0fa5cabdf875b92c84698eaa037aaf6c85198a1bb24ea969527c8cf1f34a8391",
+         intel: "63615c2f3a3e0eb12aad20f39cdb493d7163cb3bf3819004a0a62afa07a574dc"
 
   url "https://github.com/Voxelum/x-minecraft-launcher/releases/download/v#{version}/xmcl-#{version}-#{arch}.dmg",
       verified: "github.com/Voxelum/x-minecraft-launcher/"
